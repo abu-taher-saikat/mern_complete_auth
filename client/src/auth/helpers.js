0a57b7de46d95ee726e35ae00e63c658b250aff2
saikat1095 <mdabuthaersaikat@gmail.com>
2021-05-29 01:33:22 +0600

    auth helper added, cookie, localstorage fuction added

---
+++ client/src/auth/helpers.js
@@ -0,0 +1,64 @@
+import cookie from 'js-cookie';
+
+// Set The Cookie
+export const setCookie = (key , value) => {
+    if(window !== 'undefined'){
+        cookie.set(key, value,{
+            expires : 1
+        })
+    }
+}
+
+// Remove from cookie 
+export const removeCookie = (key) => {
+    if(window !== 'undefined'){
+        cookie.remove(key,{
+            expires : 1
+        })
+    }
+}
+
+// Get Frm cookie such as stored token.
+// will be useful when we need to make request to server with token.
+export const getCookie = (key) => {
+    if(window !== 'undefined'){
+        return cookie.get(key)
+    }
+}
+
+// Set in localstorage 
+export const setLocalStorage = (key , value) => {
+    if(window !== 'undefined'){
+        localStorage.getItem(key, JSON.stringify(value))
+    }
+}
+
+// remove from localstorage. 
+export const removeLocalStorage = (key) => {
+    if(window !== 'undefined'){
+        localStorage.removeItem(key)
+    }
+}
+
+
+// authenticate user by passing data to cookie and localstorage during signin.
+export const authenticate = (response , next)=> {
+    console.log(`Authenticate helper on sign in response`, response )
+    setCookie('token', response.data.token)
+    setLocalStorage('user', response.data.user);
+    next();
+}
+
+// access user info from localstorage. 
+export const isAuth = () => {
+    if(window !== 'undefined'){
+        const cookieChecked = getCookie('token');
+        if(cookieChecked){
+            if(localStorage.getItem('user')){
+                return JSON.parse(localStorage.getItem('user'));
+            }else{
+                return false;
+            }
+        }
+    }
+}
