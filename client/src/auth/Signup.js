8a99870411b2fafac1ee91ab64c2d0b1a230b605
saikat1095 <mdabuthaersaikat@gmail.com>
2021-05-28 23:07:01 +0600

    front end sign up route added so far

---
+++ client/src/auth/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../core/Layout'
+import {ToastContainer, toast} from 'react-toastify';
+
+
+const Signup = () => {
+    return (
+        <Layout>
+            <ToastContainer></ToastContainer>
+            <h1>Sign up</h1>
+        </Layout>
+    )
+}
+
+export default Signup
